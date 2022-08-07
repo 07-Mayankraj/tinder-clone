350f802e41c32f94a3e7180678f9deec7a9cafa5
07.Mayank-raj <2003.mayank.raj@gmail.com>
2022-08-07 16:26:28 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    {/* types of components
+        functional componets 
+        class components
+    
+    */}
+
+
+    {/* header */}
+    
+
+    </>
+  );
+}
+
+export default App;
